ce4b11acd65c1d247c64cadc18fa2403395dff7f
Higink <higink@gmail.com>
2025-10-16 23:56:35 +0200

    Add basic core logic

--- lib/core.ts
+++ lib/core.ts
@@ -3,31 +3,50 @@ import normalizeUrl from "./normalizeUrl";
 import {getErrorMessage} from "../utils/error";
 
 function scan(url: string) {
+    let domain;
     try {
-        const domain = new URL(url).hostname.replace(/^www\./, '');
-
-        // Clean URL
-        const startURL = normalizeUrl(url);
-        if (!startURL) {
-            return {
-                success: false,
-                message: `Invalid URL: ${url}`,
-            };
-        }
-
-
-        return {
-            success: true,
-            url: startURL,
-            domain: domain
-        };
+        domain = new URL(url).hostname.replace(/^www\./, '');
     } catch (error) {
         return {
             success: false,
-            message: `Invalid URL: ${url}`,
             error: getErrorMessage(error)
         };
     }
+
+    const urlsToVisit: Set<string> = new Set();
+    const visitedUrls: Set<string> = new Set();
+    const visitedUrlsData: { url: string; status: number }[] = [];
+
+    // @TODO utiliser plus tard la fonction DETECT NEW URL qui normalise
+    urlsToVisit.add(url)
+
+
+    while (urlsToVisit.size > 0) {
+        const currentUrl = urlsToVisit.values().next().value;
+        console.log(colors.blue(`[DEBUG] Visiting: ${currentUrl}`)); //@TODO remove
+
+        if (typeof currentUrl !== "string") {
+            console.error(colors.red(`[ERROR] Invalid URL: ${currentUrl}`));
+            // @ts-ignore
+            urlsToVisit.delete(currentUrl); //@TODO fix ts-ignore
+            continue;
+        }
+
+        urlsToVisit.delete(currentUrl);
+        visitedUrls.add(currentUrl);
+
+        // @TODO
+
+        visitedUrlsData.push({url: currentUrl, status: 200});
+    }
+
+
+    return {
+        success: true,
+        domain: domain
+    };
+
+
 }
 
 export default scan;
